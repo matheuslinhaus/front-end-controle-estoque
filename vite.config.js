6b1f58f104fd1afa8d70383daa5c75c725a38044
Matheus R Linhaus <matheuslinhauss@hotmail.com>
2024-12-16 21:38:24 -0300

    tentando fazer o deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/front-end-controle-estoque/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
